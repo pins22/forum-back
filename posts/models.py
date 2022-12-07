1387e99b46719a7762ad0a8c7e678361d708a9cd
zrezke <jeretina.filip@gmail.com>
2022-12-07 17:50:04 +0100

    merge

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=1024)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_changed = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return "Post: " + self.title
+
+
+class Reply(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_changed = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return "Reply: " + self.body
--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,24 @@
-from django.shortcuts import render
-
+from rest_framework.decorators import permission_classes
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from .models import Post
+from .serializers import PostSerializer
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
 # Create your views here.
 
+
+class PostListApiView(ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    authentication_classes = (SessionAuthentication,)
+
+    # @permission_classes([AllowAny])
+    def get(self, request, *args, **kwargs):
+        # is user loged in?
+        if request.user.is_authenticated:
+            print("User is authenticated")
+        else:
+            print("User is not authenticated")
+        return Response(self.serializer_class(self.get_queryset(), many=True).data)
