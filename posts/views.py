43e6e8fa46ba485fc9f905f9a305321425a7ffc4
zrezke <jeretina.filip@gmail.com>
2022-12-07 20:08:43 +0100

    fix some author related stuff

--- posts/views.py
+++ posts/views.py
@@ -15,6 +15,15 @@ class PostListApiView(ListCreateAPIView):
     pagination_class = Pagination
     permission_classes = (IsAuthenticatedOrReadOnly,)
 
+    def post(self, request, *args, **kwargs):
+        data = request.data
+        data['author'] = request.user.id
+        serializer = PostSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+
 
 class PostRetrieveUpdateDeleteApiView(RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
@@ -26,3 +35,9 @@ class PostRetrieveUpdateDeleteApiView(RetrieveUpdateDestroyAPIView):
         if instance.author != request.user:
             return Response({'message': 'You are not the author of this post.'}, status=403)
         return super().delete(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if instance.author != request.user:
+            return Response({'message': 'You are not the author of this post.'}, status=403)
+        return super().patch(request, *args, **kwargs)
